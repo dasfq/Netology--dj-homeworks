7df21517e18dfae6139bc5bf54366544d96b601a
Dasfq <denis-966@mail.ru>
2019-12-17 00:29:02 +0500

    Task #3

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,4 +1,9 @@
 from django import template
+from datetime import datetime
+from dateutil import parser
+import time
+from math import *
+from itertools import chain
 
 
 register = template.Library()
@@ -6,17 +11,48 @@ register = template.Library()
 
 @register.filter
 def format_date(value):
-    # Ваш код
-    return value
+    now = parser.parse(time.ctime(time.time()))
+    post_time = parser.parse(time.ctime(value))
+    time_delta = now - post_time
+    if (time_delta.seconds / 60) < 10:
+        return "Только что"
+    elif (time_delta.seconds / 60 / 60) < 24:
+        return f'{ceil(time_delta.seconds / 60 / 60)} часов назад'
+    elif (time_delta.seconds / 60 / 60) >= 24:
+        return post_time
 
 
-# необходимо добавить фильтр для поля `score`
-
+@register.filter(name='score_filter')
+def score_filter(value, default):
+    if value == False:
+        return default
+    else:
+        if value < -5:
+            return "Всё плохо"
+        elif -5 <= value < 5:
+            return "Нейтрально"
+        elif value >= 5:
+            return "Хорошо"
 
 @register.filter
 def format_num_comments(value):
-    # Ваш код
+    if value == 0:
+        return "Оставьте комментарий"
+    elif 0 < value < 50:
+        return value
+    elif value >= 50:
+        return "50+"
     return value
 
 
-
+@register.filter
+def format_selftext(value, count):
+    text = value.split()
+    length = len(text)
+    start_index = count
+    finish_index = length - count
+    replace_with = ['...']
+    if length < count * 2 + 2:
+        return value
+    else:
+        return " ".join(list(chain(text[:start_index], replace_with, text[finish_index:])))
