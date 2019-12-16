27692c11d7a344a5a5942a9ecc19f0b0eee7bcf4
Dasfq <denis-966@mail.ru>
2019-12-17 00:16:45 +0500

    Task #2

--- dynamic-templates/task2/app/urls.py
+++ dynamic-templates/task2/app/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from .views import home_view, about_view, contacts_view, examples_view
 from django.urls import path
+from django.contrib import admin
 
-from .views import home_view, about_view, contacts_view, \
-    examples_view
+
 
 
 urlpatterns = [
