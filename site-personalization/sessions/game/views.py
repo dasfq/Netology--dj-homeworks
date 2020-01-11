16a6fc2f7b672961997cd28b7b2adc0c73530d2d
Dasfq <denis-966@mail.ru>
2020-01-11 22:34:07 +0500

    Task #1

--- site-personalization/sessions/game/views.py
+++ site-personalization/sessions/game/views.py
@@ -22,67 +22,62 @@ def start_host_game(request):
 def is_player_exist(request):
     return True if 'player_id' in request.session.keys() else False
 
-def is_player_host(request):
-    player_id = request.session['player_id']
-    is_host = Player.objects.filter(id=player_id).values()[0]['is_host']
-    if is_host:
+def choose_template(request):
+    if is_player_host(request):
         template = 'home.html'
     else:
         template = 'input.html'
     return template
+
+def is_player_host(request):
+    player_id = request.session['player_id']
+    is_host = Player.objects.filter(id=player_id).values()[0]['is_host']
+    return is_host
 
 def is_current_game(request):
     games_list = Game.objects.all().order_by('-id')
     return request.session.get('game_id', 0) == games_list[0].id
 
 def show_home(request):
-    template_name = 'home.html'
-    # нужно проверять, что game_id  в сессии совпадает с текущей игрой game_id в базе
     games_list = Game.objects.all().order_by('-id')
     if games_list.count() == 0:
-        # создаём новую. и записываем данные в сессию.
-        print('Игр не было - создаём')
         current_game = start_host_game(request)
-        template_name = is_player_host(request)
+        template_name = choose_template(request)
     else:
-        print('Игра уже идёт')
-        # Делаем последнюю созданную игру текущей.
         latest_game_id = games_list[0].id
         current_game_id = request.session.get('game_id', latest_game_id)
-        current_game = PlayerGameInfo.objects.get(game__id=current_game_id)
+        current_game = PlayerGameInfo.objects.all().get(game__id=current_game_id)
+        print(f" - id из сессии {current_game_id}, id из базы {latest_game_id}")
         if current_game.game.is_finished == False:
-            print('Есть незавершённая игра')
+            print(f'игра продолжается')
             if not is_player_exist(request):
-                print('Создали нового игрока-угадывателя, т.к. у него не было сессии.')
                 new_player = Player.objects.create()
                 request.session['player_id'] = new_player.id
                 request.session['game_id'] = current_game.game.id
-            template_name = is_player_host(request)
+                print(f'Игрока не было - создали игрока-отгадывателя{new_player.id}')
+            template_name = choose_template(request)
         else:
-            print('Последняя игра была завершена')
-            # Если игра завершена, это может быть создатель (тогда ему покажем, что число угадано) или
-            # это угадыватель. ТОгда поздравим его.
             if is_player_exist(request):
-                print('Игрок существует')
+                print(f'игрок существует')
                 if not is_player_host(request):
-                    # тут нужно ему сообщить, что он угадал число. А во 2й раз создать новую игру.
-                    template_name = is_player_host(request)
-                    request.session.pop('player_id')
-                    print('Удалили данные из сессии у НеХоста')
-                elif is_player_host(request):
-                    current_game.message = f"Число {current_game.number_hidden} было угадано c {current_game.attempt_qty} попыток."
-                    template_name=is_player_host(request)
+                    print(f'игрок не хост')
+                    # тут нужно ему сообщить, что он угадал число. А во после f5 уже создать новую игру.
+                    template_name = choose_template(request)
                     request.session.pop('player_id')
                     request.session.pop('game_id')
-                    print('Удалили данные из сессии у Хоста')
-                else:
-                    # если кто-то зайдёт из третьего браузера без сессии, создадим ему плеера.
-                    current_game = start_host_game(request)
-                    template_name = is_player_host(request)
+                    print(f'удалили данные об игре из сессии. id последней игры')
+                elif is_player_host(request):
+                    print(f'игрок - хост')
+                    current_game.message = f"Число {current_game.number_hidden} было угадано c {current_game.attempt_qty} попыток."
+                    template_name=choose_template(request)
+                    request.session.pop('player_id')
+                    request.session.pop('game_id')
+                    print(f'{is_current_game(request)} - удалили из сессии данные об игре и игроке')
             else:
-                print('Игрок не существует. Значит создаём новую игру с игроком-хостом.')
+                # если кто-то зайдёт без сессии, создадим новую игру и  плеера.
                 current_game = start_host_game(request)
-                template_name = is_player_host(request)
+                template_name = choose_template(request)
+                print(f'создаём игру и игрок становится хостом')
     if request.method == 'POST':
         print('POST')
         number_try = int(request.POST.get('number_try', 0))
@@ -102,7 +97,6 @@ def show_home(request):
 
     return render(request, template_name, context={
         "game": current_game,
-        # "is_player_host": template_name,
     })
 
 
